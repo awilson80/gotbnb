80b02de306268c4ef65851880b845dbcbbd2a5dc
unknown <alyssanwilson308@gmail.com>
2022-10-10 09:23:31 -0500

    checking in tiny change for switching setups

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,8 @@
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
 import displayImg from '../img/zaferes.png';
 import star from '../img/star.png';
 import ellipse from '../img/ellipse.png';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card-container'>
       <img
