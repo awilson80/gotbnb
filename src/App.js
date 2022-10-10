d8eeb0a9cd3b97a2be666c3aebcf122037c25c3d
unknown <alyssanwilson308@gmail.com>
2022-10-10 14:14:17 -0500

    Reworked card component and moved img folder

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Component imports
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
@@ -7,7 +8,14 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img='zaferes.png'
+        rating='5.0'
+        reviewCount={6}
+        country='USA'
+        title='Life Lessons with Katie Zaferes'
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,3 @@
-import displayImg from '../img/zaferes.png';
 import star from '../img/star.png';
 import ellipse from '../img/ellipse.png';
 
@@ -6,20 +5,20 @@ export default function Card(props) {
   return (
     <div className='card-container'>
       <img
-        src={displayImg}
-        alt='Image representing an experience or excursion'
+        src={`./img/${props.img}`}
+        alt='Example of an experience or excursion'
         className='card-image'
       />
       <div className='card-rating-container'>
         <img src={star} alt='Star rating icon' className='card-star' />
-        <p>5.0</p>
-        <p>(6)</p>
+        <p>{props.rating}</p>
+        <p>({props.reviewCount})</p>
         <img src={ellipse} className='card-ellipse' />
-        <p>USA</p>
+        <p>{props.country}</p>
       </div>
-      <p className='card-descrip'>Life lessons with Katie Zaferes</p>
+      <p className='card-descrip'>{props.title}</p>
       <p className='card-price'>
-        <strong>From $136</strong> / person
+        <strong>From ${props.price}</strong> / person
       </p>
     </div>
   );
