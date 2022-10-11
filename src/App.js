70c63069cf587a992c3cd701426e6e3ffd58ab58
awilson80 <alyssanwilson308@gmail.com>
2022-10-11 10:05:11 +0900

    Fixed image path

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 
+// Image imports
+import imgZaferes from './img/zaferes.png';
+
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Card
-        img='zaferes.png'
+        img={imgZaferes}
         rating='5.0'
         reviewCount={6}
         country='USA'
